ba7dae8679f464d340500b5d5fd93ff5a162d024
Elmer Espinoza <elmesp@gmail.com>
2018-09-22 21:37:48 -0500

    Add fix provisioner section

--- config.tf
+++ config.tf
@@ -7,12 +7,12 @@ provider "aws" {
 resource "aws_instance" "nasa_nginx" {
   ami           = "ami-0ac019f4fcb7cb7e6"
   instance_type = "t2.micro"
-}
 
-provisioner "remote-exec" {
-  inline = [
-    "sudo apt-get -y update",
-    "sudo apt-get -y install nginx",
-    "sudo service nginx start",
-  ]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
+    ]
+  }
 }
